8a6211e5b6c2591b93510c01f235127347697bad
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2023-04-25 09:50:26 -0300

    SigninPage layout

--- webapp/lib/feature/users/presentation/pages/signIn.dart
+++ webapp/lib/feature/users/presentation/pages/signIn.dart
@@ -32,50 +32,82 @@ class _SignInPageState extends State<SignInPage> {
             children: [
               Image.asset(
                 'assets/images/login.png',
-                height: 300,
-                width: 400,
+                height: 350,
+                width: 600,
               ),
               Form(
                 key: _formKey,
                 child: Column(
                   children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0, bottom: 10.0),
+                      child: SizedBox(
+                        width: 340,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[300],
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(width: 1, color: Colors.white),
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                            labelText: 'Email',
+                          ),
+                        ),
+                      ),
+                    ),
                     SizedBox(
                       width: 340,
                       child: TextFormField(
                         decoration: InputDecoration(
                           filled: true,
-                          fillColor: Colors.grey[150],
+                          fillColor: Colors.grey[300],
                           enabledBorder: OutlineInputBorder(
                             borderSide:
                                 BorderSide(width: 1, color: Colors.white),
                             borderRadius: BorderRadius.circular(25.0),
                           ),
-                          labelText: 'Nome',
+                          labelText: 'Senha',
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 60.0),
-                child: SizedBox(
-                  width: 340,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(
-                        '/SignInPage',
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
-                      ),
-                      backgroundColor: const Color(0xFF706CD8),
                     ),
-                    child: Text('LOGIN'),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: SizedBox(
+                        width: 340,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pushNamed(
+                              '/signin',
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            backgroundColor: const Color(0xFF706CD8),
+                          ),
+                          child: Text('LOGIN'),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(
+                          '/signup',
+                        );
+                      },
+                      style: TextButton.styleFrom(
+                          primary: Color(0xFF706CD8) // Text Color
+                          ),
+                      child: Text(
+                        'Você não tem conta? Cadastrar',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
