4368299315e0b707fa4036140a76ddb1f47e5d42
Bianca Nilsen <biancanilsen.entra21@gmail.com>
2023-05-04 10:19:09 -0300

    add

--- webapp/lib/main.dart
+++ webapp/lib/main.dart
@@ -17,45 +17,21 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final LocalStorage storage = new LocalStorage('token');
-  bool _isLoading = true;
-  bool _hasToken = false;
-
-  @override
-  void initState() {
-    super.initState();
-    checkPortal();
-  }
-
-  Future<void> checkPortal() async {
-    final token = storage.getItem('token');
-    setState(() {
-      _isLoading = false;
-      _hasToken = token != null;
-      print('token: $token');
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    if (_isLoading) {
-      // Exibe um indicador de progresso enquanto a verificação é feita.
-      return Container();
-    } else {
-      // Decide qual página exibir com base no token.
-      final initialRoute = _hasToken ? '/list' : '/portal';
-      return MaterialApp(
-        title: 'App RBAC',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        initialRoute: initialRoute,
-        routes: {
-          '/portal': (context) => const PortalPage(),
-          '/signin': (context) => SignInPage(),
-          '/signup': (context) => SignUpPage(),
-          '/list': (context) => ListUsersPage(),
-        },
-      );
-    }
+    return MaterialApp(
+      title: 'App RBAC',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      routes: {
+        '/': (context) => const PortalPage(),
+        '/signin': (context) => SignInPage(),
+        '/signup': (context) => SignUpPage(),
+        '/list': (context) => ListUsersPage(),
+      },
+    );
   }
 }
